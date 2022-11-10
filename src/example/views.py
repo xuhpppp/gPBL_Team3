237bb001b27405b51acac81d6e06848330d59153
xuhp <doanthephuc.ST@gmail.com>
2022-11-10 14:50:50 +0700

    create new person

--- src/example/views.py
+++ src/example/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -12,3 +14,17 @@ class ListPersonView(APIView):
         person = Person.objects.all()
         serializer = PersonSerializer(person, many=True)
         return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PersonSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return JsonResponse({
+                'message': 'Create a new person successfully'
+            }, status = status.HTTP_201_CREATED)
+        else:
+            return JsonResponse({
+                'message': 'Create a new person unsuccessfully'
+            }, status = status.HTTP_400_BAD_REQUEST)
