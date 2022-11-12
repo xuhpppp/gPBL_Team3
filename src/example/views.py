c5d326ce3c3ebdd71c725009612e1b40fc4d48c6
lehuy21 <117720362+lehuy21@users.noreply.github.com>
2022-11-12 18:50:57 +0700

    Update views.py

--- src/example/views.py
+++ src/example/views.py
@@ -29,5 +29,14 @@ class ListPersonView(APIView):
                 'message': 'Create a new person unsuccessfully'
             }, status = status.HTTP_400_BAD_REQUEST)
         
+     def delete(self, request, format=None, id=None):
+        try:
+            person = Person.objects.get(pk=id)
+            if request.method =="DELETE":
+                person.delete()
+            return Response({'massage' : 'massage deleted'})
+        except exceptions.ObjectDoesNotExist:
+            return Response({'message':'Object does not exist'}, status=status.HTTP_204_NO_CONTENT)
+        
 
     
