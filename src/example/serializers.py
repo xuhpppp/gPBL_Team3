7b027dd9c3eed3721f2dedd9e601e88c9b116fdf
xuhp <doanthephuc.ST@gmail.com>
2022-11-15 00:39:41 +0700

    put api fix conflict

--- src/example/serializers.py
+++ src/example/serializers.py
@@ -5,8 +5,4 @@ from .models import Person
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-<<<<<<< HEAD
         fields = ('name', 'age', 'address', 'phone_number', 'email', 'id_company', 'id')
-=======
-        fields = ('name', 'age', 'address', 'phone_number', 'email', 'id_company' , 'id')
->>>>>>> namntd
