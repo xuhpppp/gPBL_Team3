bde68cd077b7053a46fb759516741ed6df2af073
xuhp <doanthephuc.ST@gmail.com>
2022-12-07 00:48:47 +0700

    stream by img

--- mysite/stream/detect.py
+++ mysite/stream/detect.py
@@ -179,7 +179,8 @@ def run(
                     windows.append(p)
                     cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
                     cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                cv2.imshow(str(p), im0)
+                # cv2.imshow(str(p), im0)
+                cv2.imwrite('myfront/src/assets/demo.jpg', im0)
                 # django to process here
                 cv2.waitKey(1)  # 1 millisecond
 
